c44b91fb94290ddd3db1e5b1993678563d7df380
Mateusz Gemza <neoneroxx@gmail.com>
2020-04-15 21:17:01 +0200

    Implement Rachunek class methods

--- wst-banki/Rachunek.cpp
+++ wst-banki/Rachunek.cpp
@@ -1,1 +1,39 @@
 #include "Rachunek.h"
+
+Rachunek::Rachunek()
+{
+}
+
+Rachunek::Rachunek(NUMER_RACHUNKU numer)
+{
+	this->numer = numer;
+	/* Assign default amount*/
+	this->stan = 100.00;
+}
+
+bool Rachunek::Wplata(double kwota)
+{
+	this->stan += kwota;
+	return true;
+}
+
+bool Rachunek::Wyplata(double kwota)
+{
+	if (this->stan - kwota > 0)
+	{
+		this->stan -= kwota;
+		return true;
+	}
+
+	return false;
+}
+
+NUMER_RACHUNKU Rachunek::Numer()
+{
+	return this->numer;
+}
+
+double Rachunek::Stan()
+{
+	return this->stan;
+}
