d6e64d2eaa1b4b206f6f7f1dd30f0a71286e2fc1
Mateusz Gemza <neoneroxx@gmail.com>
2020-04-15 20:29:46 +0200

    Remove function  declaration from cpp

--- wst-banki/Rachunek.cpp
+++ wst-banki/Rachunek.cpp
@@ -1,16 +1,1 @@
 #include "Rachunek.h"
-#include "Definicje.h"
-
-class Rachunek
-{
-private:
-	NUMER_RACHUNKU numer; // numer rachunku
-	double stan; // aktualny stan rachunku
-public:
-	Rachunek(); // Standardowy konstruktor bez parametrow
-	Rachunek(NUMER_RACHUNKU numer); // Konstruktor
-	bool Wplata(double kwota); // Wplac kwote na rachunek
-	bool Wyplata(double kwota); // Pobierz kwote z rachunku
-	NUMER_RACHUNKU Numer(); // Pokaz numer rachunku
-	double Stan(); // Pokaz stan rachunku
-};
